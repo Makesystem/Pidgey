bc2a68f74117dcb9caba579be466ce1143119cf3
Richeli.vargas <Richeli.vargas@DEV02.makesystem.srv>
2019-03-22 11:30:33 -0300

    Nova classe

--- pidgey/src/main/java/com/makesystem/pidgey/console/ConsoleHelper.java
+++ pidgey/src/main/java/com/makesystem/pidgey/console/ConsoleHelper.java
@@ -14,7 +14,9 @@ import java.util.Arrays;
  */
 public final class ConsoleHelper {
 
-    private static final int DIVIDER_WIDTH = 72;
+    public static final int DIVIDER_WIDTH = 72;
+    public static final String DIVIDER_CHARACTER = "-";
+    public static final ConsoleColor DIVIDER_COLOR = ConsoleColor.BLACK_BOLD;
 
     public static void print(final HasPrintfSupport... values) {
         Arrays.asList(values).forEach(value -> System.out.printf(value.getPrintfFormat(), value.getPrintfArgs()));
@@ -30,31 +32,35 @@ public final class ConsoleHelper {
     }
 
     public static void printDivider() {
-        printDivider("-", DIVIDER_WIDTH, ConsoleColor.BLACK_BOLD);
+        printDivider(DIVIDER_CHARACTER, DIVIDER_WIDTH, DIVIDER_COLOR);
     }
 
     public static void printDivider(final String dividerChar) {
-        printDivider(dividerChar, DIVIDER_WIDTH, ConsoleColor.BLACK_BOLD);
+        printDivider(dividerChar, DIVIDER_WIDTH, DIVIDER_COLOR);
     }
 
     public static void printDivider(final int width) {
-        printDivider("-", width, ConsoleColor.BLACK_BOLD);
+        printDivider(DIVIDER_CHARACTER, width, DIVIDER_COLOR);
     }
 
     public static void printDivider(final ConsoleColor... colors) {
-        printDivider("-", DIVIDER_WIDTH, colors);
+        printDivider(DIVIDER_CHARACTER, DIVIDER_WIDTH, colors);
     }
 
     public static void printDivider(final int width, final ConsoleColor... colors) {
-        printDivider("-", width, colors);
+        printDivider(DIVIDER_CHARACTER, width, colors);
     }
 
-    public static void printDivider(final String dividerChar, final int width, final ConsoleColor... colors) {
+    public static void printDivider(final String dividerChar, final int width, final ConsoleColor... colors) {        
+        final ConsoleValue divider = new ConsoleValue(divider(dividerChar, width), colors);
+        printlnEach(divider);
+    }
+    
+    public static String divider(final String dividerChar, final int width){
         final StringBuilder builder = new StringBuilder();
         for (int i = 0; i < width; i++) {
             builder.append(dividerChar);
         }
-        final ConsoleValue divider = new ConsoleValue(builder, colors);
-        printlnEach(divider);
+        return builder.toString();
     }
 }
