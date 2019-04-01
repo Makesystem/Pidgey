a7c2b12fdace31cd3cd06dfda933bb29c577d451
Richeli.vargas <Richeli.vargas@DEV02.makesystem.srv>
2019-04-01 15:50:36 -0300

    Ajuste

--- pidgey/src/main/java/com/makesystem/pidgey/lang/Range.java
+++ pidgey/src/main/java/com/makesystem/pidgey/lang/Range.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.makesystem.pidgey.language;
+package com.makesystem.pidgey.lang;
 
 import java.io.Serializable;
 
