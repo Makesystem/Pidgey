622247defda06436df4ba8221467134c9e2b21a7
Richeli.vargas <Richeli.vargas@DEV02.makesystem.srv>
2019-05-21 16:44:01 -0300

    Test with native javascript code

---
+++ pidgey/src/main/java/com/makesystem/pidgey/io/InnetAddressHelperGWT.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.makesystem.pidgey.io;
+
+/**
+ *
+ * @author Richeli.vargas
+ */
+public class InnetAddressHelperGWT {
+
+    public static void getLocalIp(final GetIpHandler handler){
+        try {
+            _getLocalIp(handler);
+        } catch (final Throwable throwable){
+            handler.onFailure(throwable);
+        }
+    }
+            
+    private static native void _getLocalIp(final GetIpHandler handler) /*-{	
+        window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for firefox and chrome
+	var pc = new RTCPeerConnection({
+            iceServers : []
+        }), noop = function() {};
+        
+        pc.createDataChannel(""); //create a bogus data channel
+        pc.createOffer(pc.setLocalDescription.bind(pc), noop); // create offer and set local description
+        pc.onicecandidate = function(ice) { //listen for candidate events        
+            if (!ice || !ice.candidate || !ice.candidate.candidate)
+                return;
+			
+            var local_ip = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+            handler.@com.makesystem.pidgey.io.GetIpHandler::onSuccess(Ljava/lang/String;)(local_ip);		
+            pc.onicecandidate = noop;
+            
+	};
+    }-*/;
+
+    public static void getPublicIp(final GetIpHandler handler){
+        try {
+            _getPublicIp(handler);
+        } catch (final Throwable throwable){
+            handler.onFailure(throwable);
+        }
+    }
+    
+    private static native void _getPublicIp(final GetIpHandler handler)/*-{
+        function getIp(callback) {
+            function response(s) {
+		callback(window.userip);
+
+		s.onload = s.onerror = null;
+		document.body.removeChild(s);
+            }
+
+            function trigger() {
+		window.userip = false;
+
+		var s = document.createElement("script");
+		s.async = true;
+		s.onload = function() {
+                    response(s);
+		};
+		s.onerror = function() {
+                    response(s);
+		};
+
+		s.src = "https://l2.io/ip.js?var=userip";
+		document.body.appendChild(s);
+            }
+
+            if (/^(interactive|complete)$/i.test(document.readyState)) {
+                trigger();
+            } else {
+		document.addEventListener('DOMContentLoaded', trigger);
+            }
+	}
+
+        getIp(function(public_ip) {
+            handler.@com.makesystem.pidgey.io.GetIpHandler::onSuccess(Ljava/lang/String;)(public_ip);			
+        });
+
+    }-*/;
+
+}
---
+++ pidgey/src/main/java/com/makesystem/pidgey/io/InnetAddressHelperJRE.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.makesystem.pidgey.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+
+/**
+ *
+ * @author Richeli.vargas
+ */
+public class InnetAddressHelperJRE {
+
+    public static void getLocalIp(final GetIpHandler handler) {
+        try {
+            final InetAddress inetAddress = InetAddress.getLocalHost();
+            handler.onSuccess(inetAddress.getHostAddress());
+        } catch (@SuppressWarnings("UseSpecificCatch") final Throwable error) {
+            handler.onFailure(error);
+        }
+    }
+
+    public static void getPublicIp(final GetIpHandler handler) {
+        try {
+            final URL whatismyip = new URL("http://checkip.amazonaws.com");
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(new InputStreamReader(
+                        whatismyip.openStream()));
+                final String ip = in.readLine();
+                handler.onSuccess(ip);
+            } finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException error) {
+                        handler.onFailure(error);
+                    }
+                }
+            }
+        } catch (@SuppressWarnings("UseSpecificCatch") final Throwable error) {
+            handler.onFailure(error);
+        }
+    }
+}
